5f7025bee7b154703b95046b9dd79275a730efb4
Kaiz <realkaiz7@gmail.com>
2022-04-25 07:05:25 +0800

    update

--- script.js
+++ script.js
@@ -1,186 +1,157 @@
-let TOKEN = ""; // Auth token
-const client_id = "344cdc5aaf1e48d598aedc7bc81c6e3f"; // Your client ID
-const redirect_uri = window.location; // Your Redirect URI - Make sure to set this Redirect URI in your Spotify app settings.
-const scope = "user-read-private user-read-email user-top-read"; // A space separated scopes.
+var TOKEN = ""; // The token. Will be updated later.
+var client_id = "581c7ad32a9d49aca66b20d3511dd59b"; // Your client ID
+var redirect_uri = window.location; // The current page URL
+var scope = "user-read-private user-read-email user-top-read"; // A space separated scopes.
 function getToken() {
   var url = "https://accounts.spotify.com/authorize";
   url += "?response_type=token";
   url += "&client_id=" + encodeURIComponent(client_id);
   url += "&scope=" + encodeURIComponent(scope);
   url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+  console.log(url);
   window.open(url, "_self");
 }
 
-function extractTokenFromURL() {
-  const hash = window.location.hash;
+function extractTokenFromURI() {
+  var hash = window.location.hash;
   if (hash && hash.includes("access_token")) {
-    const url = hash.replace("#access_token=", "");
-    const chunks = url.split("&");
-    const token = chunks[0];
+    var url = hash.replace("#access_token=", "");
+    var chunks = url.split("&");
+    var token = chunks[0];
     return token;
   }
-  return null;
-}
-
-function generateCard(image, name, description, href) {
-  return `
-    <a class="card" href="${href}" target="_blank">
-        <img
-            src="${image}"
-            alt="peaceful piano"
-            srcset=""
-        />
-        <span class="mdi mdi-play mdi-36px"></span>
-        <div class="title">${name}</div>
-        <div class="subtitle">${description}</div>
-    </a>
-    `;
-}
-
-function displayUserTopItems(data) {
-  const sectionTopItems = document.querySelector("#your-top-items");
-  const sectionTopItemsTitle = sectionTopItems.querySelector(".title");
-  const sectionTopItemsSubtitle = sectionTopItems.querySelector(".subtitle");
-  const sectionTopItemsWrapper = sectionTopItems.querySelector(".card-wrapper");
-  sectionTopItemsTitle.textContent = "Your Top Items";
-  sectionTopItemsSubtitle.textContent = "Based on your recent listening";
-  for (let i = 0; i < data.items.length; i++) {
-    const track = data.items[i];
-
-    const image = track.album.images[1].url;
-    const name = track.name;
-    const description = track.album.artists
-      .map((artist) => artist.name)
-      .join(", ");
-    const href = track.album.external_urls.spotify;
-
-    sectionTopItemsWrapper.innerHTML += generateCard(
-      image,
-      name,
-      description,
-      href
-    );
-  }
-}
-
-function displayNewReleases(data) {
-  const sectionNewReleases = document.querySelector("#new-releases");
-  const sectionNewReleasesTitle = sectionNewReleases.querySelector(".title");
-  const sectionNewReleasesSubtitle =
-    sectionNewReleases.querySelector(".subtitle");
-  const sectionNewReleasesWrapper =
-    sectionNewReleases.querySelector(".card-wrapper");
-  sectionNewReleasesTitle.textContent = "Popular New Releases";
-  sectionNewReleasesSubtitle.textContent = "";
-  for (let i = 0; i < data.albums.items.length; i++) {
-    const track = data.albums.items[i];
-
-    const image = track.images[1].url;
-    const name = track.name;
-    const description = track.artists.map((artist) => artist.name).join(", ");
-    const href = track.external_urls.spotify;
-
-    sectionNewReleasesWrapper.innerHTML += generateCard(
-      image,
-      name,
-      description,
-      href
-    );
-  }
-}
-
-function displayFeaturedPlaylists(data) {
-  const sectionFeaturedPlaylists = document.querySelector(
-    "#featured-playlists"
-  );
-  const sectionFeaturedPlaylistsTitle =
-    sectionFeaturedPlaylists.querySelector(".title");
-  const sectionFeaturedPlaylistsSubtitle =
-    sectionFeaturedPlaylists.querySelector(".subtitle");
-  const sectionFeaturedPlaylistsWrapper =
-    sectionFeaturedPlaylists.querySelector(".card-wrapper");
-  sectionFeaturedPlaylistsTitle.textContent = data.message;
-  sectionFeaturedPlaylistsSubtitle.textContent = "";
-  for (let i = 0; i < data.playlists.items.length; i++) {
-    const track = data.playlists.items[i];
-
-    const image = track.images[0].url;
-    const name = track.name;
-    const description = track.description;
-    const href = track.external_urls.spotify;
-
-    sectionFeaturedPlaylistsWrapper.innerHTML += generateCard(
-      image,
-      name,
-      description,
-      href
-    );
-  }
+  return null; // Return null if there's no token
 }
 
 async function fetchUserTopItems() {
   try {
-    const data = await fetch(
-      "https://api.spotify.com/v1/me/top/tracks?limit=6",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + TOKEN,
-        },
-      }
-    );
-    const json = await data.json();
-    displayUserTopItems(json);
+    var endpoint = "https://api.spotify.com/v1/me/top/tracks";
+    var response = await fetch(endpoint + "?limit=6", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + TOKEN,
+      },
+    });
+    var data = await response.json();
+    console.log("User top items", data);
+    displayUserTopItems(data); // Display user top items
   } catch (error) {
     alert("Something went wrong.");
-    console.error(error);
+    console.log(error);
   }
 }
 
 async function fetchNewReleases() {
   try {
-    const data = await fetch(
-      "https://api.spotify.com/v1/browse/new-releases?limit=6",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + TOKEN,
-        },
-      }
-    );
-    const json = await data.json();
-    displayNewReleases(json);
+    var endpoint = "https://api.spotify.com/v1/browse/new-releases";
+    var response = await fetch(endpoint + "?limit=6", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + TOKEN,
+      },
+    });
+    var data = await response.json();
+    console.log("New releases", data);
+    displayNewReleases(data); // Display new releases
   } catch (error) {
     alert("Something went wrong.");
-    console.error(error);
+    console.log(error);
   }
 }
 
 async function fetchFeaturedPlaylists() {
   try {
-    const data = await fetch(
-      "https://api.spotify.com/v1/browse/featured-playlists?limit=6",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + TOKEN,
-        },
-      }
-    );
-    const json = await data.json();
-    displayFeaturedPlaylists(json);
+    var endpoint = "https://api.spotify.com/v1/browse/featured-playlists";
+    var response = await fetch(endpoint + "?limit=6", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + TOKEN,
+      },
+    });
+    var data = await response.json();
+    console.log("Featured playlists", data);
+    displayFeaturedPlaylists(data); // Display featured playlists
   } catch (error) {
     alert("Something went wrong.");
-    console.error(error);
+    console.log(error);
+  }
+}
+
+function generateCard(image, title, subtitle, href) {
+  return `
+      <a class="card" href="${href}" target="_blank">
+          <img
+              src="${image}"
+              alt="peaceful piano"
+              srcset=""
+          />
+          <span class="mdi mdi-play mdi-36px"></span>
+          <div class="title">${title}</div>
+          <div class="subtitle">${subtitle}</div>
+      </a>
+      `;
+}
+
+function displayUserTopItems(data) {
+  var section = document.querySelector("#your-top-items");
+  var sectionTitle = section.querySelector(".title");
+  var sectionSubtitle = section.querySelector(".subtitle");
+  var sectionWrapper = section.querySelector(".card-wrapper");
+  sectionTitle.textContent = "Your Top Items";
+  sectionSubtitle.textContent = "Based on your recent listening";
+  for (let i = 0; i < data.items.length; i++) {
+    var track = data.items[i];
+
+    var image = track.album.images[1].url;
+    var title = track.name;
+    var subtitle = track.album.artists[0].name;
+    var href = track.album.external_urls.spotify;
+
+    sectionWrapper.innerHTML += generateCard(image, title, subtitle, href);
+  }
+}
+function displayNewReleases(data) {
+  var section = document.querySelector("#new-releases");
+  var sectionTitle = section.querySelector(".title");
+  var sectionSubtitle = section.querySelector(".subtitle");
+  var sectionWrapper = section.querySelector(".card-wrapper");
+  sectionTitle.textContent = "New Releases";
+  sectionSubtitle.textContent = "New releases from Spotify";
+  for (let i = 0; i < data.albums.items.length; i++) {
+    var track = data.albums.items[i];
+
+    var image = track.images[1].url;
+    var title = track.name;
+    var subtitle = track.artists[0].name;
+    var href = track.external_urls.spotify;
+
+    sectionWrapper.innerHTML += generateCard(image, title, subtitle, href);
+  }
+}
+function displayFeaturedPlaylists(data) {
+  var section = document.querySelector("#featured-playlists");
+  var sectionTitle = section.querySelector(".title");
+  var sectionSubtitle = section.querySelector(".subtitle");
+  var sectionWrapper = section.querySelector(".card-wrapper");
+  sectionTitle.textContent = "Featured Playlists";
+  sectionSubtitle.textContent = "Featured playlists from Spotify";
+  for (let i = 0; i < data.playlists.items.length; i++) {
+    var track = data.playlists.items[i];
+
+    var image = track.images[0].url;
+    var title = track.name;
+    var subtitle = track.description;
+    var href = track.external_urls.spotify;
+
+    sectionWrapper.innerHTML += generateCard(image, title, subtitle, href);
   }
 }
 
 window.addEventListener("load", function () {
-  TOKEN = extractTokenFromURL();
+  TOKEN = extractTokenFromURI();
   if (TOKEN) {
+    console.log("Token", TOKEN);
+    // fetch the endpoints
     fetchUserTopItems();
     fetchNewReleases();
     fetchFeaturedPlaylists();
